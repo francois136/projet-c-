23b5a3fece7d72e0c017f641a1943158c5853a17
leila0987 <leila.mabrouk.2002@gmail.com>
2025-01-07 11:30:57 +0100

    Update BandeDessinee.h

--- BandeDessinee.h
+++ BandeDessinee.h
@@ -9,14 +9,14 @@ class BandeDessinee:
     private:
         string dessinateur ;     // Nom de l'illustrateur
     public:
-        BandeDessinee(const string& auteur, const string& titre, const string& editeur, const string& isbn, const string& dessinateur);
-        BandeDessinee(const Livre& livre, const string& dessinateur); // Constructeur de recopie
+        BandeDessinee(string auteur, string titre, string editeur, string isbn, string dessinateur);
+        BandeDessinee(Livre& livre, string& dessinateur); // Constructeur de recopie
 
         string getDessinateur(); //getter
         void setDessinateur(string); //setter
         
         // Méthode pour afficher les détails spécifiques à une bande dessinée
-        void afficher_details() const override;
+        void afficher_details();
 
         // Destructeur
         virtual ~BandeDessinee();
