a449118c7f887ede61cfce700d420ce244c47a32
leila0987 <leila.mabrouk.2002@gmail.com>
2025-01-07 09:21:39 +0100

    Update Bibliotheque.h

--- Bibliotheque.h
+++ Bibliotheque.h
@@ -1,1 +1,15 @@
+#ifndef BIBLIOTHEQUE_H_
+#define BIBLIOTHEQUE_H_
+#include <iostream>
+using namespace std;
+#include "Livre.h"
+#include <stdexcept>
+class Livre;
 
+class Bibliotheque{
+    private:
+        String nom;
+        String adresse;
+        int code;
+        int capaciteMax;
+        int nombreLivres;
