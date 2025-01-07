41731a16b2d8dd01ecc0b7ffc3cccaa7255a59df
francois136 <112487083+francois136@users.noreply.github.com>
2025-01-07 11:09:38 +0100

    Update Adherent.h

--- Adherent.h
+++ Adherent.h
@@ -1,6 +1,6 @@
 #ifndef ADHERENT_H
 #include ADHERENT_H
-#include 
+#include <string>
 using namespace std;
 
 class Adherent{
@@ -10,8 +10,29 @@ class Adherent{
     string adresse;
     int numero;
     Bibliotheque bibliotheque;
-    
-    
+    Livre* Livresempruntes;
+    int maxLivres;
+  public:
+    Adherent(string,string,string,int,Bibliotheque,int);
+    Adherent();
+    void emprunter(int);
+    void retourner(int);
+
+    string gentom();
+    string getprenom();
+    string getadress();
+    int getnumero();
+    Bibliotheque getbibliotheque();
+    Livre* getlivresempruntes();
+    int getmaxLivres();
+
+    void sentom(string);
+    void setprenom(string);
+    void setadress(string);
+    void setnumero(int);
+    void setbibliotheque(Bibliothéque);
+    doid setlivresempruntes(Livre*);
+    void setmaxLivres(int);
 }
 
 #endif
