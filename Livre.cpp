805c49989edc58364daa6bc639325da15fa79b3e
francois136 <112487083+francois136@users.noreply.github.com>
2025-01-07 09:16:25 +0100

    Livre.cpp

---
+++ Livre.cpp
@@ -0,0 +1,22 @@
+#include "Livre.h"
+#include <iostream>
+using namespace std;
+
+void Livre::changer_etat(nouvel_etat){
+    etat = nouvel_etat;
+}
+
+void Livre::afficher_details(){
+    cout << "code : " << code << endl;
+    cout << "auteur : " << auteur << endl;
+    cout << "titre : " << titre << endl;
+    cout << "e"
+}
+        int code;
+        string auteur;
+        string titre;
+        string editeur;
+        string isbn;
+        string public;
+        strign etat;
+        Bibliotheque bibliothequeOrigine;
