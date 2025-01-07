0f68fdcbfc755858948cd25b39a8351fa802248f
leila0987 <leila.mabrouk.2002@gmail.com>
2025-01-07 11:18:14 +0100

    Update BandeDessinee.cpp

--- BandeDessinee.cpp
+++ BandeDessinee.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include "BandeDessinee.h"
 
+using namespace std;
+
+// Constructeur prenant les attributs spécifiques et appelant le constructeur de Livre
+BandeDessinee::BandeDessinee(string auteur, string titre, string editeur, string isbn, string dessinateur)
+    : Livre(auteur, titre, editeur, isbn), dessinateur(dessinateur) {}
+
+// Constructeur de recopie
+BandeDessinee::BandeDessinee(Livre* livre, string dessinateur)
+    : Livre(livre), dessinateur(dessinateur) {}
+
+// Getter pour le genre
+string BandeDessinee::getDessinateur() {
+    return dessinateur;
+}
+
+// Setter pour le genre
+void BandeDessinee::setDessinateur(string nouveauDessinateur) {
+	dessinateur = nouveauDessinateur;
+}
+
+// Afficher les détails spécifiques à une bande dessinée
+void BandeDessinee::afficher_details() const {
+    Livre::afficher_details(); // Appel de la méthode de la classe de base
+    cout << "Dessinateur : " << dessinateur << endl;
+}
+
+// Destructeur
+BandeDessinee::~BandeDessinee() {}
