3e8817ff6940e25fccefc187746f3555f55fd888
francois136 <112487083+francois136@users.noreply.github.com>
2025-01-07 11:49:07 +0100

    Create Album.h

---
+++ Album.h
@@ -0,0 +1,15 @@
+#ifndef ALBUM_H
+#include ALBUM_H
+using namespace std;
+
+class Album{
+  public:
+    string typeillustration;
+  private:
+    Album();
+    Album(Livre, string);
+    strign gettypeillustration();
+    void settypeillustration();
+}
+
+#endif
