e39007e2f8a871045abd0c389dba94b268fcf9e2
francois136 <112487083+francois136@users.noreply.github.com>
2025-01-07 10:59:55 +0100

    Update Adherent.h

--- Adherent.h
+++ Adherent.h
@@ -1,5 +1,17 @@
 #ifndef ADHERENT_H
 #include ADHERENT_H
+#include 
+using namespace std;
 
+class Adherent{
+  private:
+    string nom;
+    string prenom;
+    string adresse;
+    int numero;
+    Bibliotheque bibliotheque;
+    
+    
+}
 
 #endif
