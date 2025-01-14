38509a34fb2d3cd14571742042884dba2ea43afb
francois136 <112487083+francois136@users.noreply.github.com>
2025-01-14 14:47:56 +0100

    Create Adherent.cpp

---
+++ Adherent.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "adherent.h"
+#include <string>
+using namespace std;
+
+Adherent::Adherent(string n,string p,string a,int n,Bibliotheque b,int m){
+    nom = n;
+    prenom = p;
+    adresse = a;
+    numero = n;
+    bibliotheque = b;
+    maxLivres = m;
+    Livresempruntes = {};
+}
+
+Adherent::Adherent(Bibliotheque b){
+    nom = "";
+    prenom = "";
+    adresse = "";
+    numero = 0;
+    bibliotheque = b;
+    Livresempruntes = {};
+}
+
+void Adherent::emprunter(Livre livre) {
+    if (livresEmpruntes.size() < maxLivres) {
+        livresEmpruntes.push_back(livre);
+    } else {
+        throw runtime_error("Nombre maximum de livres empruntés atteint.");
+    }
+}
+
+void Adherent::retourner(Livre) {
+    livresEmpruntes.remove(livre);
+}
+
+string Adherent::gentom(){
+    return nom;
+}
+
+string Adherent::getprenom(){
+    return prenom;
+}
+
+string Adherent::getadress(){
+    return adresse;
+}
+
+int Adherent::getnumero(){
+    return numero;
+}
+
+Bibliotheque Adherent::getbibliotheque(){
+    return bibliotheque;
+}
+
+list<Livre> Adherent::getlivresempruntes(){
+    return livresempruntes;
+}
+
+int Adherent::getmaxLivres(){
+    return maxlivres;
+}
+
+void Adherent::sentnom(string n){
+    nom = n
+}
+
+void Adherent::setprenom(string p){
+    prenom = p;
+}
+
+void Adherent::setadresse(string a){
+    adresse = a;
+}
+
+void Adherent::setnumero(int n){
+    numero = n;
+}
+
+void Adherent::setbibliotheque(Biblioteque b){
+    bibliotheque = b;
+}
+
+void Adherent::setlivresempruntes(list<Livre> l){
+    Livresempruntes = l;
+}
+
+void Adherent::setmaxLivres(int m){
+    maxLivres = m;
+}
