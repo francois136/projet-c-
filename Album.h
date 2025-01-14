338d8275d5cc4355848485fe8cfe9706863c71ad
leila0987 <leila.mabrouk.2002@gmail.com>
2025-01-14 14:38:18 +0100

    Update Album.h

--- Album.h
+++ Album.h
@@ -1,15 +1,22 @@
-#ifndef ALBUM_H
-#include ALBUM_H
+#ifndef ALBUM_H_
+#define ALBUM_H_
+#include <iostream>
 using namespace std;
+#include "Livre.h"
 
-class Album{
-  public:
-    string typeillustration;
-  private:
-    Album();
-    Album(Livre, string);
-    strign gettypeillustration();
-    void settypeillustration();
-}
+class Album:
+		public Livre {
+    private:
+        string illustration ;     // Nom de l'illustrateur
+    public:
+        Album(string auteur, string titre, string editeur, string isbn, string illustration);
+        Album(Livre& livre, string& illustration); // Constructeur de recopie
 
-#endif
+        string getillustration(); //getter
+        void setsillustration(string); //setter
+
+        // Destructeur
+        virtual ~Album();
+};
+
+#endif /* ALBUM_H_ */
